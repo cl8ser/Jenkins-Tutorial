b57cd7b1a8a6c7c7174dc3c2e5601f0bc004689b
cl8ser <30623757+cl8ser@users.noreply.github.com>
2017-08-02 00:04:03 +0300

    Initial Commit

---
+++ payslip/src/main/java/payslip/GratuityComponent.java
@@ -0,0 +1,22 @@
+package payslip;
+
+//666
+
+public class GratuityComponent {
+	
+	   private double gratuity;
+
+	   // constructor
+	   public GratuityComponent(){
+		  
+		  FixedComponent var = new FixedComponent();
+	      this.gratuity = (var.basicPay()*15)/(26*12);
+	   }
+	      
+	   // prints the variable pay
+	   public double printGratuity(){
+	      System.out.println(gratuity);
+	      return gratuity;
+	   }   
+	} 
+
